e09a1922becbb5653b8a6b3b093c969e9cc2d7d9
Keerthi Kalyaan <keerthi.kalyaan2004@gmail.com>
2021-06-26 12:14:09 -0700

    Co-authored-by: Aaaron <aaaron24@users.noreply.github.com>

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import "./Home.css";
 const Home = () => {
-  return <div>Home Page</div>;
+  return <div className="hero">
+  <div className="hero-inner">
+
+    <div className="hero-text">
+      <h1 className= "title">The Sky's<br/>Not the<br/>Limit</h1>
+      
+
+   
+    </div>
+  </div>
+</div>;
 };
 
 export default Home;
