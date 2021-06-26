8faecd976164c1adabb3403265122d66a9926ef6
Aaron T <aronthakur@gmail.com>
2021-06-26 12:29:49 -0700

    Navbar  added with other small changes

---
+++ src/Components/Navbar/NavbarData.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+import styles from "./Navbar.module.css";
+
+export const NavbarData = [
+  {
+    title: "Contact",
+    path: "/contact",
+    // cName: `${styles.navTextTop}`,
+  },
+  {
+    title: "Donate",
+    path: "/donate",
+    // cName: `${styles.navTextTop}`,
+  },
+  {
+    title: "Login",
+    path: "/login",
+    // cName: `${styles.navTextTop}`,
+  },
+];
