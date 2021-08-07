25c57e2617663f2e9e3993c4b2ed43478ce0a8b2
Aaditya Doiphode <aadityad27@gmail.com>
2021-08-06 20:44:41 -0700

    updated code

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import styles from "./Home.module.css";
-import comet from "./comet.jpeg";
+import comet from "../../Assets/Images/comet.jpeg";
+import hands from "../../Assets/Images/hands.jpeg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -56,83 +57,11 @@ const Home = () => {
           })}
         </div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "bisque",
-          width: "100%",
-          marginTop: "400px",
-          height: "100%",
-          padding: "50px 0 50px 0",
-        }}
-      >
-        <div className="row" style={{ padding: "2px" }}>
-          <div
-            className="col-12 col-sm-8 col-md-6 col-lg-4"
-            style={{ marginTop: "20px" }}
-          >
-            <div
-              className="card"
-              style={{
-                padding: "20px",
-                paddingLeft: "10px",
-                backgroundColor: "rgb(215,235,242)",
-              }}
-            >
-              <h1
-                style={{ fontSize: "40px", fontWeight: "bold", margin: "auto" }}
-              >
-                ~ OUR MISSION ~
-              </h1>
-              <p style={{ width: "78%", fontSize: "18px", margin: "auto" }}>
-                As an international organization of volunteers, our goal is to
-                help students across the world by providing access to online
-                learning. Our services are meant to assist both children and
-                their parents by bridging the gap in education during these
-                difficult times. At-home boredom is a reality for many children,
-                and our organization strives to create activities that ignite
-                their creativity and encourage their love for learning.
-              </p>
-              <br />
-              <button
-                className={styles.contactusbutton}
-                style={{ margin: "auto" }}
-              >
-                Contact Us!
-              </button>
-            </div>
-          </div>
-          <div
-            className="col-12 col-sm-8 col-md-6 col-lg-4"
-            style={{ marginTop: "20px" }}
-          >
-            <div className="card">
-              <div>
-                <img
-                  src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                  alt="image"
-                  width="480"
-                  height="440"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div style={{ display: "flex" }}>
-          <div className={styles.imagecards}>
-            <img src="" alt="image" />
-          </div>
-          <div
-            className={styles.infocards}
-            style={{ backgroundColor: "lightblue" }}
-          >
-            <h1 style={{ fontSize: "40px", fontWeight: "bold" }}>
-              ~ OUR MISSION ~
-            </h1>
-            <p style={{ width: "78%", fontSize: "18px", margin: "auto" }}>
+      <div className={styles.bottomContainer}>
+        <div className={styles.cardContainer}>
+          <div className={styles.ourMission}>
+            <h1>~Our Mission~</h1>
+            <p>
               As an international organization of volunteers, our goal is to
               help students across the world by providing access to online
               learning. Our services are meant to assist both children and their
@@ -141,8 +70,29 @@ const Home = () => {
               organization strives to create activities that ignite their
               creativity and encourage their love for learning.
             </p>
-            <br />
-            <button className={styles.contactusbutton}>Contact Us!</button>
+            <Link to="#">
+              <button className={styles.contactUsButton}>Contact Us</button>
+            </Link>
+          </div>
+          <div>
+            <img
+              src={hands}
+              alt="hands"
+              style={{ height: "100%", width: "100%", objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.ourMission}></div>
+          <div className={styles.ourMission}>
+            <h1>~Our Recruiting~</h1>
+            <p>
+              During the months of June and July, Reach For The Stars will be
+              recruiting members for our Core Team.
+            </p>
+            <br />​
+            <p>
+              This is the team that runs the entire organization. For more
+              information about each position, check the document linked below.
+            </p>
           </div>
         </div>
       </div>
