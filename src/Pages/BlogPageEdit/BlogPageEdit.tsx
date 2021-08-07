169ffd9f2c6c981f0ad77e6c9d061d387a325185
Aaditya Doiphode <aadityad27@gmail.com>
2021-08-07 00:17:51 -0700

    working home page scroll and all cards updated

--- src/Pages/BlogPageEdit/BlogPageEdit.tsx
+++ src/Pages/BlogPageEdit/BlogPageEdit.tsx
@@ -1,73 +1,141 @@
-import React from "react";
-import styles from "./BlogPageEdit.module.css"
+import React, { useRef, MutableRefObject, useState } from "react";
+import styles from "./BlogPageEdit.module.css";
 import * as FaIcons from "react-icons/fa";
-import {Editor}  from '@tinymce/tinymce-react';
+import { Editor } from "@tinymce/tinymce-react";
+import { addBlog } from "../../Backend/db/dbfunctions";
 const BlogPageEdit = () => {
-    const API_KEY = process.env.REACT_APP_TINY_CLOUD_MCE_TEXT_EDITOR_API_KEY;
-    return(
-       
-        <div className={styles.contain}>
-            <a className = {styles.back}  href="index.html" ><FaIcons.FaArrowLeft className={styles.icon}  /> Back </a>
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" width="100%" alt = "card" />
-            <div className={styles.blog}>
-            <h1 className={styles.title}>Title</h1>
-                <div className={styles.content}>
-                <div className={styles.info}>
-                <p>Author</p>
-                <p>Date</p>
-                </div>
-               <div className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatem quibusdam, error quaerat, officiis reiciendis cupiditate magni voluptates earum inventore recusandae. Hic in magni porro molestiae. Adipisci quae deleniti et!
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro aspernatur mollitia, tempore, quos quam facere nesciunt quo id reiciendis praesentium molestiae, laudantium sed ducimus cum tenetur recusandae voluptas enim asperiores!
-               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est a placeat quidem eligendi quam provident numquam culpa sed recusandae. A provident asperiores reprehenderit vitae delectus nemo error rerum omnis ducimus.
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti neque praesentium? Atque recusandae libero doloribus possimus voluptate itaque nesciunt, consectetur nam illum, repellendus quo provident voluptas maiores eos ut.
-               <br></br>
-               <br></br>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolor adipisci voluptas, nostrum repellat ipsa, tempore ratione similique recusandae perspiciatis id illum mollitia blanditiis ipsam autem delectus esse maxime quam.
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestiae error provident dolor iure, reiciendis beatae unde doloribus deleniti sapiente praesentium numquam quam, architecto aspernatur, a eligendi nobis aliquam facere!
-               </div>
-                </div>
-            </div>
-            
-            <h1 className={styles.edit}>Edit:</h1>
-            <form className={styles.textEditor}>
-            <Editor
-       
-        apiKey={API_KEY}
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist autolink lists link image', 
-            'charmap print preview anchor help',
-            'searchreplace visualblocks code',
-            'insertdatetime media table paste wordcount'
-          ],
-          toolbar:
-            'undo redo | formatselect | bold italic | \
-            alignleft aligncenter alignright | \
-            bullist numlist outdent indent | help'
-        }}
-       
-      />
-      <div>
-    
-      <div className="form-group" style={{marginTop:"20px"}}>
-    <label >Edit Author:</label>
-    <input type="email" className="form-control" placeholder="Enter New Author.." />
-  </div>
-  <div className="form-group">
-    <label >Image Src Url:</label>
-    <input type="password" className="form-control" placeholder="Enter New Image Src Url..." />
-  </div>
-      </div>
-      
-      <button className={styles.button}>Save</button>
-      <button className={styles.button2}>Cancel</button>
-      </form>
-      </div>
-        
-        
+  const API_KEY = process.env.REACT_APP_TINY_CLOUD_MCE_TEXT_EDITOR_API_KEY;
+  const authorRef = useRef() as MutableRefObject<any>;
+  const urlRef = useRef() as MutableRefObject<any>;
+  const titleRef = useRef() as MutableRefObject<any>;
+  const [content, setContentState] = useState("");
+
+  const handleEditorChange = (e: any) => {
+    console.log(
+      "Content was updated:",
+      e.target.getContent({ format: "text" })
     );
+    setContentState(e.target.getContent({ format: "text" }));
+    console.log(content, " this is your content");
+  };
+
+  const setBlogData = () => {
+    console.log(
+      authorRef.current.value,
+      " ",
+      urlRef.current.value,
+      " ",
+      content,
+      " ",
+      titleRef.current.value
+    );
+  };
+
+  <input type="text" name="username" />;
+  return (
+    <div className={styles.contain}>
+      <a className={styles.back} href="index.html">
+        <FaIcons.FaArrowLeft className={styles.icon} /> Back{" "}
+      </a>
+      <img
+        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        width="100%"
+        alt="card"
+      />
+      <div className={styles.blog}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.content}>
+          <div className={styles.info}>
+            <p>Author</p>
+            <p>Date</p>
+          </div>
+          <div className={styles.text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            voluptatem quibusdam, error quaerat, officiis reiciendis cupiditate
+            magni voluptates earum inventore recusandae. Hic in magni porro
+            molestiae. Adipisci quae deleniti et! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Porro aspernatur mollitia, tempore,
+            quos quam facere nesciunt quo id reiciendis praesentium molestiae,
+            laudantium sed ducimus cum tenetur recusandae voluptas enim
+            asperiores! Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Est a placeat quidem eligendi quam provident numquam culpa sed
+            recusandae. A provident asperiores reprehenderit vitae delectus nemo
+            error rerum omnis ducimus. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Exercitationem corrupti neque praesentium? Atque
+            recusandae libero doloribus possimus voluptate itaque nesciunt,
+            consectetur nam illum, repellendus quo provident voluptas maiores
+            eos ut.
+            <br></br>
+            <br></br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem dolor adipisci voluptas, nostrum repellat ipsa,
+            tempore ratione similique recusandae perspiciatis id illum mollitia
+            blanditiis ipsam autem delectus esse maxime quam. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Eveniet molestiae error
+            provident dolor iure, reiciendis beatae unde doloribus deleniti
+            sapiente praesentium numquam quam, architecto aspernatur, a eligendi
+            nobis aliquam facere!
+          </div>
+        </div>
+      </div>
+
+      <h1 className={styles.edit}>Edit:</h1>
+      <form className={styles.textEditor}>
+        <div className="form-group" style={{ marginTop: "20px" }}>
+          <label>Edit Title:</label>
+          <input
+            ref={titleRef}
+            type="text"
+            className="form-control"
+            placeholder="Enter Your Title.."
+          />
+        </div>
+        <Editor
+          apiKey={API_KEY}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist autolink lists link image",
+              "charmap print preview anchor help",
+              "searchreplace visualblocks code",
+              "insertdatetime media table paste wordcount",
+            ],
+            toolbar:
+              "undo redo | formatselect | bold italic | \
+            alignleft aligncenter alignright | \
+            bullist numlist outdent indent | help",
+          }}
+          onChange={handleEditorChange}
+        />
+        <div>
+          <div className="form-group" style={{ marginTop: "20px" }}>
+            <label>Edit Author:</label>
+            <input
+              ref={authorRef}
+              type="email"
+              className="form-control"
+              placeholder="Enter New Author.."
+            />
+          </div>
+          <div className="form-group">
+            <label>Image Src Url:</label>
+            <input
+              ref={urlRef}
+              type="text"
+              className="form-control"
+              placeholder="Enter New Image Src Url..."
+            />
+          </div>
+        </div>
+
+        <button type="submit" className={styles.button}>
+          Save
+        </button>
+        <button className={styles.button2}>Cancel</button>
+      </form>
+    </div>
+  );
 };
 
 export default BlogPageEdit;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import styles from "./Home.module.css";
 import comet from "../../Assets/Images/comet.jpeg";
 import hands from "../../Assets/Images/hands.jpeg";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
 const Home = () => {
   const myRef = useRef();
@@ -11,26 +11,36 @@ const Home = () => {
 
   // run this function from an event handler or an effect to execute scroll
 
+  // const handleOnClick = (event: any) => {
+  //   //.current is verification that your element has rendered
+  //   if (this.myDivToFocus.current) {
+  //     this.myDivToFocus.current.scrollIntoView({
+  //       behavior: "smooth",
+  //       block: "nearest",
+  //     });
+  //   }
+  // };
+
   const items = [
     {
       title: "Our Mission",
-      path: "#",
+      path: "ourMission",
     },
     {
       title: "Recruiting",
-      path: "#",
+      path: "recruiting",
     },
     {
       title: "Tutoring Resources",
-      path: "#",
+      path: "tutoringResources",
     },
     {
       title: "Our Programs",
-      path: "#",
+      path: "ourPrograms",
     },
     {
       title: "Blog",
-      path: "#",
+      path: "blog",
     },
   ];
 
@@ -48,7 +58,12 @@ const Home = () => {
         <div className={styles.refBar}>
           {items.map((item, index) => {
             return (
-              <Link to={item.path}>
+              <Link
+                activeClass="active"
+                to={item.path}
+                spy={true}
+                smooth={true}
+              >
                 <button className={styles.button} key={index}>
                   {item.title}
                 </button>
@@ -59,7 +74,7 @@ const Home = () => {
       </div>
       <div className={styles.bottomContainer}>
         <div className={styles.cardContainer}>
-          <div className={styles.ourMission}>
+          <div id="ourMission" className={styles.ourMission}>
             <h1>~Our Mission~</h1>
             <p>
               As an international organization of volunteers, our goal is to
@@ -81,8 +96,14 @@ const Home = () => {
               style={{ height: "100%", width: "100%", objectFit: "cover" }}
             />
           </div>
-          <div className={styles.ourMission}></div>
-          <div className={styles.ourMission}>
+          <div>
+            <img
+              src={hands}
+              alt="hands"
+              style={{ maxHeight: "100%", width: "100%", objectFit: "cover" }}
+            />
+          </div>
+          <div id="recruiting" className={styles.ourMission}>
             <h1>~Our Recruiting~</h1>
             <p>
               During the months of June and July, Reach For The Stars will be
@@ -93,6 +114,127 @@ const Home = () => {
               This is the team that runs the entire organization. For more
               information about each position, check the document linked below.
             </p>
+          </div>
+        </div>
+        <div id="tutoringResources" className={styles.tutoringResources}>
+          <h1>Tutoring Resources</h1>
+          <h2>Peer Tutoring, Private Tutoring, AP Peer Classes</h2>
+          <button className={styles.registerbuttonstyling}>
+            Register Here
+          </button>
+          <div className={styles.cardContainer}>
+            <div className={styles.tutoringResourcesCards}>
+              <img
+                src={hands}
+                alt="hands"
+                style={{ maxHeight: "40%", width: "100%", objectFit: "cover" }}
+              />
+              <div
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                  padding: "20px 40px 40px",
+                }}
+              >
+                <h1>Peer Tutoring</h1>
+                <p>
+                  At Reach For The Stars, we are dedicated to making your
+                  child's learning experience feel like a real class. Our peer
+                  tutoring involves our tutors in a class with 2 or more
+                  children. The students can make new friends and connect with
+                  people all over the world while receiving a quality education.
+                </p>
+                <button className={styles.contactUsButton}>Learn More</button>
+              </div>
+            </div>
+            <div className={styles.tutoringResourcesCards}>
+              <img
+                src={hands}
+                alt="hands"
+                style={{ maxHeight: "40%", width: "100%", objectFit: "cover" }}
+              />
+              <div
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                  padding: "20px 40px 40px",
+                }}
+              >
+                <h1>Private Tutoring</h1>
+                <p>
+                  Private Tutoring is done between 1 tutor and 1 student. The
+                  child gets more time with the tutor and can ask more
+                  questions, as only one person is attending to the child.
+                </p>
+                <button className={styles.contactUsButton}>Learn More</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="ourPrograms" className={styles.ourProgramsContainer}>
+          <div style={{ textAlign: "center", marginBottom: "30px" }}>
+            <h1>Our Programs</h1>
+            <h2>What we offer</h2>
+          </div>
+          <div className={styles.cardContainer}>
+            <div className={styles.summerCamp}>
+              <h1>SUMMER CAMP</h1>
+              <ol>
+                <li>
+                  There will be two cohorts (week of July 19th-23rd and week of
+                  August 2nd-6th)
+                </li>
+                <li>
+                  Classes last from Monday to Friday and are from 10 AM-11 AM or
+                  11 AM-12 PM EST.
+                </li>
+                <li>
+                  You can enroll in a maximum of 2 classes for every cohort you
+                  enroll in!
+                </li>
+                <li>
+                  Class times are different depending on which class you take.
+                  They will either be from 10:00 AM EST- 11:00 AM EST or 11:00
+                  AM EST- 12:00 PM EST. Keep note of the times listed below for
+                  cohort 1!
+                </li>
+              </ol>
+              <Link to="#">
+                <button className={styles.contactUsButton}>Contact Us</button>
+              </Link>
+            </div>
+            <div>
+              <img
+                src={hands}
+                alt="hands"
+                style={{ height: "100%", width: "100%", objectFit: "cover" }}
+              />
+            </div>
+            <div>
+              <img
+                src={hands}
+                alt="hands"
+                style={{ height: "100%", width: "100%", objectFit: "cover" }}
+              />
+            </div>
+            <div className={styles.peerClassCard}>
+              <h1>AP PEER Classes</h1>
+              <p>
+                The AP Peer Class Program is a review course for students taking
+                the May AP test. <br />
+                There are over 20 subjects for students to choose from. Our
+                tutors and TAs make sure that these students are able to score
+                high on their AP tests with review worksheets, lectures, Q&A
+                sessions, and diagnostic tests. <br />
+                Stayed tuned for our 2022 registration!
+              </p>
+            </div>
           </div>
         </div>
       </div>
