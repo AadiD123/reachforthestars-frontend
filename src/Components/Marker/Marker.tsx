3faf20b3e6f7f96c34ef034d6e85ac60842d0bea
Aaditya Doiphode <aadityad27@gmail.com>
2021-08-15 16:24:51 -0700

    working changes

--- src/Components/Marker/Marker.tsx
+++ src/Components/Marker/Marker.tsx
@@ -1,24 +1,26 @@
 import styles from "./Marker.module.css";
 
 function Marker(props: any) {
-    const {  name, text} = props;
-    
-    return (
-        <div
-        className={styles.pin}
-        style={{ backgroundColor: "blue", cursor: 'pointer' }}
-        title={name}
-        
-      >
-        
-          <div className={styles.modal} style={{width:"200px"}}>
-  <img alt = "card" className="card-img-top" src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/06/15/Chris-Pratt.jpg?width=982&height=726&auto=webp&quality=75"style={{borderRadius:"10px"}} />
-  <div className="card-body">
-    <h4 className="card-title">{name}</h4>
-    <p className="card-text" style={{fontSize:"12px"}}>{text}</p>
-    
-  </div>
-</div>
+  const { name, text } = props;
+
+  return (
+    <div
+      className={styles.pin}
+      style={{ backgroundColor: "blue", cursor: "pointer" }}
+      title={name}
+    >
+      <div className={styles.modal} style={{ width: "200px" }}>
+        <img
+          alt="card"
+          className="card-img-top"
+          src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/06/15/Chris-Pratt.jpg?width=982&height=726&auto=webp&quality=75"
+          style={{ borderRadius: "10px" }}
+        />
+        <div className="card-body">
+          <h4 className="card-title">{name}</h4>
+          <p className="card-text" style={{ fontSize: "12px" }}>
+            {text}
+          </p>
         </div>
       </div>
     </div>
